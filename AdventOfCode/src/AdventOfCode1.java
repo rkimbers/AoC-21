9597b77508ed142c40c9a81f475effde7bbbcef9
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-12-01 19:26:37 -0600

    Day 1

--- AdventOfCode/src/AdventOfCode1.java
+++ AdventOfCode/src/AdventOfCode1.java
@@ -4,39 +4,35 @@ import java.io.*;
 
 /*
   Ryan Kimberley
+  Advent of Code 12/1/21
   AP Computer Science
  */
 
 public class AdventOfCode1 {
 
     public static void main(String[] args) throws IOException {
-        
-        Scanner scanFile = new Scanner(new File("input1.txt"));
-        String input = scanFile.nextLine();
 
-        int inputLength, currentDigit, nextDigit, count = 0;
+        Scanner scanFile = new Scanner(new File("src/inputDay1.txt"));
 
-        inputLength = input.length() - 1;
+        int firstDigit = 0, secondDigit = 0, count = 0;
+        boolean readTwoLines = false;
 
+        while (scanFile.hasNextLine()) {
 
-        for (int i = 0; i < inputLength; i++){
+            firstDigit = scanFile.nextInt();
+            while (!readTwoLines && scanFile.hasNextLine()) {
+                secondDigit = scanFile.nextInt();
+                readTwoLines = true;
+            }
 
+           System.out.println(" "+firstDigit+" "+secondDigit);
 
-
-            currentDigit = input.indexOf(i);
-            nextDigit = input.indexOf(i+1);
-
-            if (nextDigit > currentDigit) count += 1;
-
+            if(firstDigit < secondDigit) count += 1;
 
 
         }
 
-        System.out.print("Count: "+count);
-
-
-
-
+        System.out.println("Count: "+count);
 
 
     }
