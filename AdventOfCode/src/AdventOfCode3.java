2d04e454f6e85d77246c6b1b66d9415c820e00f6
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-12-05 18:37:51 -0600

    Update AdventOfCode3.java
    p1 done

--- AdventOfCode/src/AdventOfCode3.java
+++ AdventOfCode/src/AdventOfCode3.java
@@ -3,6 +3,11 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+  Ryan Kimberley
+  Advent of Code 12/2/21
+  AP Computer Science
+ */
 
 public class AdventOfCode3 {
 
@@ -10,29 +15,63 @@ public class AdventOfCode3 {
 
         Scanner scanFile = new Scanner(new File("src/inputDay3.txt"));
 
-        List<Integer> numbers = new ArrayList();
-        numbers.add(scanFile.nextInt());
-        numbers.sort();
+        List<String> numbers = new ArrayList();
+
+        //numbers.sort();
 
         int gammaRate = 0;
         int epsilonRate = 0;
+        int leastCommon = 0;
+        int mostCommon = 0;
+        int sum = 0;
+
         int currentDigit;
         String currentLine;
 
         while(scanFile.hasNextLine()){
 
-            for(int i = 0; i < 12; i++){
+            numbers.add(scanFile.nextLine());
+            }
+        //numbers.forEach((n) -> System.out.println(n));
 
-                currentLine = scanFile.nextLine();
+        StringBuilder binary = new StringBuilder();
+        StringBuilder binary1 = new StringBuilder();
+System.out.println(numbers.size());
+        for(int i = 0; i<12;i++){
+            int counter0 = 0;
+            int counter1 = 0;
+            for(String num:numbers){
+                currentDigit = Integer.parseInt(num.substring(i,(i+1)));
 
-
+//                System.out.println(currentDigit+ " " + i);
+                if (currentDigit == 1){
+                    counter1 += 1;
+                }
+                else{
+                    counter0 += 1;
+                }
 
             }
-
-
-
-
+            if(counter1 > 500){
+                gammaRate = 1;
+                epsilonRate = 0;
+            }
+            else{
+                gammaRate = 0;
+                epsilonRate = 1;
+            }
+            binary.append(gammaRate);
+            binary1.append(epsilonRate);
         }
+//        System.out.println(binary);
+//        System.out.println(binary1);
+
+        int decimalNumber = Integer.parseInt(binary.toString(),2);
+        int decimalNumber1 = Integer.parseInt(binary1.toString(),2);
+        System.out.println(decimalNumber*decimalNumber1);
+
 
     }
+
 }
+
