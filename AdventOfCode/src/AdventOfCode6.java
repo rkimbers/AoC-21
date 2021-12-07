9b0154dc301da9d843031e866297c564d1ccf8d3
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-12-07 12:56:30 -0600

    Update AdventOfCode6.java

--- AdventOfCode/src/AdventOfCode6.java
+++ AdventOfCode/src/AdventOfCode6.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+import java.io.*;
+
+/*
+  Ryan Kimberley
+  Advent of Code 12/4/21
+  AP Computer Science
+ */
+
 public class AdventOfCode6 {
+    public static void main(String[]args)throws IOException{
+
+        Scanner scanFile = new Scanner(new File("src/inputDay6.txt"));
+
+
+
+    }
 }
