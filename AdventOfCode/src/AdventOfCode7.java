f6d851f956cedc1fea940ebcbf2fa056d887c364
rkimbers <89214160+rkimbers@users.noreply.github.com>
2021-12-16 20:28:33 -0600

    Completed

--- AdventOfCode/src/AdventOfCode7.java
+++ AdventOfCode/src/AdventOfCode7.java
@@ -16,12 +16,12 @@ public class AdventOfCode7 {
 
         String input = scanFile.nextLine();
 
-        double count = 0;
-        double fuelCount = 0;
-        double fuelNumber = 0;
-        double currentDigit = 0;
-        double nextFuelCount = 0;
-        double min = 0;
+        int count = 0;
+        int fuelCount = 0;
+        int fuelNumber = 0;
+        int currentDigit = 0;
+        int nextFuelCount = 0;
+        int min = 0;
 
 
         String[] numbers = input.split(",");
@@ -40,31 +40,47 @@ public class AdventOfCode7 {
             fuelNumber += 1;
             if (nextFuelCount < fuelCount){
                 min = nextFuelCount;
-                count = (fuelNumber);
+                count = (fuelNumber+1);
             }
 
         }
         System.out.println(count); //312
-        System.out.println(min); //347011
-
-        double newFuelNumber = count;
-        double newFuelCount = 0;
-        double n = 0;
-
-        for (String k : numbers) {
-
-            currentDigit = Integer.parseInt(k);
-            n = Math.abs(currentDigit - newFuelNumber);
-            double first = currentDigit*10 ;
-            double second = newFuelNumber*10;
-            newFuelCount += Math.abs(first-second);
-            //newFuelCount += (n/2)*(n+1);
-            //System.out.println(newFuelCount);
+        System.out.println("First ans "+min); //347011
 
 
+        int min1 = 0;
+        int fuelCount1 = 0;
+        int nextFuelCount1 = 0;
+        int n = 0;
+        int n1 = 0;
+        int add = 0;
+        int add1 = 0;
+        fuelNumber = count;
 
+        while(fuelNumber < 1500) {
+            fuelCount1 = 0;
+            nextFuelCount1 = 0;
+            for (String k : numbers) {
+
+                currentDigit = Integer.parseInt(k);
+                n = (Math.abs(currentDigit - fuelNumber));
+                n1 = (Math.abs(currentDigit - (fuelNumber+1)));
+
+                add = ((n*n)+n)/2;
+                add1 = ((n1*n1)+n1)/2;
+
+                fuelCount1 += add;
+                nextFuelCount1 += add1;
+
+            }
+            fuelNumber += 1;
+            if (nextFuelCount1 < fuelCount1){
+                min1 = nextFuelCount1;
+            }
         }
-        System.out.println(newFuelCount); //195195365 too high //not 3470110
+        System.out.println("Second ans "+min1);
+
+
 
 
 
